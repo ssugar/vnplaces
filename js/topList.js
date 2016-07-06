101d3be517f47ea11b1033362f75aee5045de7cc
ssugar <scottsugar@outlook.com>
2016-07-06 16:45:24 +0700

    changing width of svg table

--- js/topList.js
+++ js/topList.js
@@ -8,7 +8,7 @@ d3.tsv("assets/summarizeData.txt", function(data)
     });
 
     var fieldHeight = 30;
-    var fieldWidth = 123;
+    var fieldWidth = 124;
     var firstColMultiplier = 3;
 
     //using total data rows + 1 for header time height + 1 for padding
