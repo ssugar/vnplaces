6f058571a7a43d3f7d08c9d5e76e2e812bc80a1f
ssugar <scottsugar@outlook.com>
2016-07-06 13:21:13 +0700

    changing search to text search

--- js/gmap.js
+++ js/gmap.js
@@ -75,6 +75,7 @@ function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
         createMarker(results[i]);
+        console.log(results[i]);
     }
     }
 }
