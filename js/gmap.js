64004bd821ebe8f09ff45cc6b1faefe77edff6e7
ssugar <scottsugar@outlook.com>
2016-07-07 15:40:10 +0700

    setting website to open in new tab

--- js/gmap.js
+++ js/gmap.js
@@ -86,7 +86,7 @@ function createMarker(place) {
 
     placesList = document.getElementById('results');
 
-    placesList.innerHTML = '<p class="' + place.types[0] + '">' + place.name + '</p>';
+    placesList.innerHTML = '<br/><p class="' + place.types[0] + '">' + place.name + '</p>';
     placesList.innerHTML += '<p class="' + place.types[0] + '">' + place.formatted_address + '</p>';
 
     var deetrequest = {
@@ -99,13 +99,13 @@ function createMarker(place) {
     function deetcallback(deetplace, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
             placesList.innerHTML += '<p class="' + place.types[0] + '">' + '<a href="tel:' + deetplace.formatted_phone_number + '"><i class="material-icons">phone</i></a>' + '</p>';
-            placesList.innerHTML += '<p class="' + place.types[0] + '">' + '<a href="' + deetplace.website + '"><i class="material-icons">public</i></a>' + '</p>';
+            placesList.innerHTML += '<p class="' + place.types[0] + '">' + '<a href="' + deetplace.website + '" target="_blank"><i class="material-icons">public</i></a>' + '</p>';
             placesList.innerHTML += '<p class="' + place.types[0] + '">' + '<img src="' + photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200}) + '"></img>' + '</p>';
         }
     }
 
     google.maps.event.addListener(marker, 'click', function () {
-        infowindow.setContent('<img src="' + photos[0].getUrl({'maxWidth': 400, 'maxHeight': 400}) + '"></img>');
+        infowindow.setContent('<img src="' + photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200}) + '"></img>');
         infowindow.open(map, this);
     });
 }
