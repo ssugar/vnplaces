26168d38a36a46b0e56681733f10d1fd83885535
ssugar <scottsugar@outlook.com>
2016-07-09 13:20:24 +0700

    removing p from around the img

--- js/gmap.js
+++ js/gmap.js
@@ -111,7 +111,7 @@ function createMarker(place) {
                 photoHtml = '<img class="imgSlider" src="' + deetplace.photos[0].getUrl({'maxWidth': 100, 'maxHeight': 100}) + '"></img>' + '<img class="imgSlider" src="' + deetplace.photos[1].getUrl({'maxWidth': 100, 'maxHeight': 100}) + '"></img>' + '<img class="imgSlider" src="' + deetplace.photos[2].getUrl({'maxWidth': 100, 'maxHeight': 100}) + '"></img>' + '<img class="imgSlider" src="' + deetplace.photos[3].getUrl({'maxWidth': 100, 'maxHeight': 100}) + '"></img>';
                 placesList.innerHTML += '<p class="' + place.types[0] + '">' + '<a href="tel:' + deetplace.formatted_phone_number + '"><i class="material-icons">phone</i></a> <a href="' + deetplace.website + '" target="_blank"><i class="material-icons">public</i></a>' + '</p>';
                 placesList.innerHTML += '<a class="w3-btn-floating-left" onclick="plusDivs(-1)">&#10094;</a>' + '<a class="w3-btn-floating-right" onclick="plusDivs(+1)">&#10095;</a>'
-                placesList.innerHTML += '<div class="photoDisplay"><p class="' + place.types[0] + '">' + photoHtml + '</p></div>';
+                placesList.innerHTML += '<div class="photoDisplay">' + photoHtml + '</div>';
                 initSlider();
             }
         }
