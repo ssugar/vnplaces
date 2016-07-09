feacecc4873640a83af855840300622e144f25b8
ssugar <scottsugar@outlook.com>
2016-07-09 10:59:50 +0700

    fixing slider code

--- js/gmap.js
+++ js/gmap.js
@@ -104,14 +104,16 @@ function createMarker(place) {
 
             if (deetplace.photos.length < 4){
                 photoHtml = '<img class="imgSlider" src="' + deetplace.photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200}) + '"></img>';
+                placesList.innerHTML += '<p class="' + place.types[0] + '">' + '<a href="tel:' + deetplace.formatted_phone_number + '"><i class="material-icons">phone</i></a> <a href="' + deetplace.website + '" target="_blank"><i class="material-icons">public</i></a>' + '</p>';
+                placesList.innerHTML += '<p class="' + place.types[0] + '">' + photoHtml + '</p>';
             }
             else {
                 photoHtml = '<img class="imgSlider" src="' + deetplace.photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200}) + '"></img>' + '<img class="imgSlider" src="' + deetplace.photos[1].getUrl({'maxWidth': 200, 'maxHeight': 200}) + '"></img>' + '<img class="imgSlider" src="' + deetplace.photos[2].getUrl({'maxWidth': 200, 'maxHeight': 200}) + '"></img>' + '<img class="imgSlider" src="' + deetplace.photos[3].getUrl({'maxWidth': 200, 'maxHeight': 200}) + '"></img>';
+                placesList.innerHTML += '<p class="' + place.types[0] + '">' + '<a href="tel:' + deetplace.formatted_phone_number + '"><i class="material-icons">phone</i></a> <a href="' + deetplace.website + '" target="_blank"><i class="material-icons">public</i></a>' + '</p>';
+                placesList.innerHTML += '<a class="w3-btn-floating" onclick="plusDivs(-1)">&#10094;</a>' + '<a class="w3-btn-floating" onclick="plusDivs(+1)">&#10095;</a>'
+                placesList.innerHTML += '<p class="' + place.types[0] + '">' + photoHtml + '</p>';
                 initSlider();
             }
-            placesList.innerHTML += '<p class="' + place.types[0] + '">' + '<a href="tel:' + deetplace.formatted_phone_number + '"><i class="material-icons">phone</i></a> <a href="' + deetplace.website + '" target="_blank"><i class="material-icons">public</i></a>' + '</p>';
-            placesList.innerHTML += '<a class="w3-btn-floating" onclick="plusDivs(-1)">&#10094;</a>' + '<a class="w3-btn-floating" onclick="plusDivs(+1)">&#10095;</a>'
-            placesList.innerHTML += '<p class="' + place.types[0] + '">' + photoHtml + '</p>';
         }
     }
 
@@ -129,12 +131,14 @@ function initSlider(){
 }
 
 function plusDivs(n) {
+    console.log(slideIndex += n);
     showDivs(slideIndex += n);
 }
 
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("imgSlider");
+    console.log(x.length);
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length} ;
     for (i = 0; i < x.length; i++) {
