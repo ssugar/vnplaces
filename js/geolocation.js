cf7977605cfd9f0b1c87024be8c8a00f513c47d6
ssugar <scottsugar@outlook.com>
2016-07-10 18:06:18 +0700

    going back to custom geolocation code

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,2 +1,43 @@
-var GeoMarker = new GeolocationMarker(map);
+var userMark;
 
+function getLocation() {
+    if (navigator.geolocation) {
+        var geoPos = navigator.geolocation.watchPosition(showPosition);
+        //var geoPos = navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        return "Geolocation is not supported by this browser.";
+    }
+}
+    
+function showPosition(position) {
+    //console.log(position.coords);
+    userMarker(position);
+}
+
+function userMarker(currentLoc) {
+    var lat = currentLoc.coords.latitude;
+    var lng = currentLoc.coords.longitude;
+    var userLatlng = new google.maps.LatLng(lat, lng);
+
+    var userIcon = {
+        url: 'https://chadkillingsworth.github.io/geolocation-marker/images/gpsloc.png',
+        scaledSize: new google.maps.Size(17, 17),
+        origin: new google.maps.Point(0,0),
+        anchor: new google.maps.Point(0, 0)
+    };
+
+    userMark = new google.maps.Marker({
+        map: map,
+        position: userLatlng,
+        icon: userIcon,      
+        title: "Your location"
+    });
+    userMark.setPosition(userLatlng);
+}
+
+function centerOnUserMark(){
+    map.setCenter(userMark.getPosition());
+}
+
+getLocation();
+centerOnUserMark();
