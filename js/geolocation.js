b2d1086e48fa5cc8d065472c3b46406ea6e8a2b8
ssugar <scottsugar@outlook.com>
2016-07-10 16:12:18 +0700

    fixing up geolocation code

--- js/geolocation.js
+++ js/geolocation.js
@@ -2,8 +2,8 @@ var userMark;
 
 function getLocation() {
     if (navigator.geolocation) {
-        //var geoPos = navigator.geolocation.watchPosition(showPosition);
-        var geoPos = navigator.geolocation.getCurrentPosition(showPosition);
+        var geoPos = navigator.geolocation.watchPosition(showPosition);
+        //var geoPos = navigator.geolocation.getCurrentPosition(showPosition);
     } else { 
         return "Geolocation is not supported by this browser.";
     }
