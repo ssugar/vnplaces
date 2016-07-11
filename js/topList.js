e31d9b3ca437fdaad201955ba2784dd59a35aedd
ssugar <scottsugar@outlook.com>
2016-07-11 21:48:48 +0700

    splitting filter from list code

--- js/topList.js
+++ js/topList.js
@@ -227,42 +227,5 @@ function clearTable(){
     d3.select(".rowsGrp").html("");
 }
 
-function initFilterAndCount(filterThresh){
-    var filterDiv = d3.select(".slideFilter");
-
-    var filterLabel = filterDiv.append("label")
-        .attr("for", "filter-input");
-
-    filterLabel.append("span")
-        .attr("id", "filter-pre-label")
-        .text('+1 > ');
-
-    filterLabel.append("span")
-        .attr("id", "filter-value")
-        .text(filterThresh);
-
-    filterDiv.append("input")
-        .attr("id", "filter-input")
-        .attr("type", "range")
-        .attr("step", "100")
-        .attr("min", "1000")
-        .attr("max", "5000")
-        .attr("style", "width=100%")
-        .attr("value", +filterThresh);
-
-    filterDiv.append("span")
-        .attr("id", "filter-pre-count")
-        .text(" = ");
-
-    filterDiv.append("span")
-        .attr("id", "rest-count");
-
-    filterDiv.append("span")
-        .attr("id", "filter-post-count")
-        .html('<i class="material-icons">restaurant</i>');
-}
-
-initFilterAndCount(filterVal);
-
 initTopList();
 
