fa655fca160dcea5f0d0c421d5d6fed5cb5956d5
ssugar <scottsugar@outlook.com>
2016-07-13 12:43:56 +0700

    add timezone

--- js/summaryDetail.js
+++ js/summaryDetail.js
@@ -2,7 +2,7 @@ function refreshSummaryDetail()
 {
     d3.text("assets/summarizeDetail.txt", function(data) {
         d3.select(".summaryDetail").append("span")
-        .text('Last data summary: ' + data);
+        .text(' Last data summary: ' + data + ' ICT ');
     });
 }
 
