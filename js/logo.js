f65474d6238afdc788f0024ab0d2efb5ec51e6f6
ssugar <scottsugar@outlook.com>
2016-07-16 11:44:29 +0700

    updating logo

--- js/logo.js
+++ js/logo.js
@@ -8,25 +8,31 @@ function generateLogo(){
     var maxY = 56;
     var margin = 5;
 
+    var loopLimit = Math.floor((maxY - (margin*2)) / (boxSize + spacer));
+
     var svgContainer = d3.select(".fixedHeader").append("svg")
         .attr("width", 100)
         .attr("height", 56);
-    
-    //svgContainer.append("circle")
-    //    .attr("cx", 20)
-    //    .attr("cy", 20)
-    //    .attr("r", 10);
-
 
     //drawing E
-    var loopLimit = Math.floor((maxY - (margin*2)) / (boxSize + spacer));
-
+    //First Line
     for(i = 0; i < loopLimit; i++) {
         svgContainer.append("rect")
             .attr("x", startX + margin)
             .attr("y", (startY + margin + (boxSize * i) + (spacer*i)))
             .attr("width", boxSize)
             .attr("height", boxSize);
+    }
+
+    //Second Line
+    for(i = 0; i < loopLimit; i++) {
+        if(i%2 == 0){  //only write on even loops to look like an E.
+            svgContainer.append("rect")
+                .attr("x", startX + margin + boxSize + spacer)
+                .attr("y", (startY + margin + (boxSize * i) + (spacer*i)))
+                .attr("width", boxSize)
+                .attr("height", boxSize);
+        }
     }
 }
 
