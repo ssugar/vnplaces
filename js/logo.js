84400a1fbe3becbb50a3c3103e51c09699fafedf
ssugar <scottsugar@outlook.com>
2016-07-16 10:51:13 +0700

    updating logo

--- js/logo.js
+++ js/logo.js
@@ -2,13 +2,18 @@
 function generateLogo(){
     var svgContainer = d3.select(".fixedHeader").append("svg")
         .attr("width", 100)
-        .attr("height", 40)
-        .attr("position", "absolute");
+        .attr("height", 40);
     
-    var circle = svgContainer.append("circle")
+    svgContainer.append("circle")
         .attr("cx", 20)
         .attr("cy", 20)
         .attr("r", 10);
+
+    svgContainer.append("circle")
+        .attr("cx", 30)
+        .attr("cy", 30)
+        .attr("r", 10);
+
 }
 
 generateLogo();
