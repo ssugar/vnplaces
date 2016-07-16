84195457d5f5818af32d0d2cff4f5f01af249ced
ssugar <scottsugar@outlook.com>
2016-07-16 10:55:11 +0700

    updating logo

--- js/logo.js
+++ js/logo.js
@@ -4,21 +4,16 @@ function generateLogo(){
         .attr("width", 100)
         .attr("height", 40);
     
-    svgContainer.append("circle")
-        .attr("cx", 20)
-        .attr("cy", 20)
-        .attr("r", 10);
-
-    svgContainer.append("circle")
-        .attr("cx", 30)
-        .attr("cy", 30)
-        .attr("r", 10);
+    //svgContainer.append("circle")
+    //    .attr("cx", 20)
+    //    .attr("cy", 20)
+    //    .attr("r", 10);
 
     svgContainer.append("rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", 3)
-        .attr("height", 3);
+        .attr("x", 10)
+        .attr("y", 10)
+        .attr("width", 5)
+        .attr("height", 5);
 }
 
 generateLogo();
