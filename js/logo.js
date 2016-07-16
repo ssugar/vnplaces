74c579efae2d31cee76b3715ffb7df61c76b07f4
ssugar <scottsugar@outlook.com>
2016-07-16 17:52:18 +0700

    changing random colors to grayscale

--- js/logo.js
+++ js/logo.js
@@ -68,8 +68,10 @@ function drawBox(){
         var circle = d3.select(this);
         (function repeat() {
             circle = circle.transition()
+                .attr("r", logoboxSize/1.25)
                 .style({fill: randomColor})
             .transition()
+                .attr("r", logoboxSize/1.5)
                 .style({fill: randomColor})
                 .each("end", repeat);
         })();
