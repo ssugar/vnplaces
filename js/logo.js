437981f96eaa78ec3c27c7ec6d75c217dc9b63c1
ssugar <scottsugar@outlook.com>
2016-07-16 15:19:06 +0700

    updating logo

--- js/logo.js
+++ js/logo.js
@@ -42,7 +42,7 @@ function drawA(){
             startY = startY + boxSize + spacer;
         }
         startY = 0;
-        startX = startX + boxSize - spacer;
+        startX = startX + boxSize + spacer/2;
     }
 }
 
